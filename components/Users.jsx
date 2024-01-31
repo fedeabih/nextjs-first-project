2e232cd5c2f4f0aff49293420e2f1edef266f931
Federico Abihaggle <federico.abihaggle@gmail.com>
2024-01-31 23:19:30 +0100

    First project configuration

---
+++ components/Users.jsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+function Users({ users }) {
+    return (
+        <ul>
+        {users.map(user => (
+            <Link
+            href={`/users/${user.id}`}>
+                <li key={user.id} className="bg-slate-400 mb-2 p-4 rounded-md text-black flex justify-between">
+                    <div>
+                    <h5 className="font-bold">{user.id} {user.first_name} {user.last_name}</h5>
+                    <p className="text-slate-100">Email: {user.email}</p>
+                    </div>
+                    <img src={user.avatar} className="rounded-full w-20"></img>
+                </li>
+            </Link>
+        ))}
+      </ul>
+    );
+}
+
+export default Users;
